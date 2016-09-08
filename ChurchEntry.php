e3491c14b3695bc577b07c5cebc4ea1e1e455028
David Marshall Berg <dberg918@gmail.com>
2016-09-09 06:28:05 +0900

    add ChurchEntry class for index and modify results page

---
+++ ChurchEntry.php
@@ -0,0 +1,30 @@
+<?php
+class ChurchEntry
+{
+    public $churchname, $denomination, $prefecture, $city, $gmap, $website,
+           $servday, $servtime;
+
+    public function displayName()
+    {
+        print $this->churchname;
+    }
+    public function displayChurch()
+    {
+        print  '<div class="church-wrapper">
+                <div class="church-profile">
+                <h1><a href="'$this->website'">'$this->churchname'</a></h1>
+                <h2>'$this->denomination'</h2>
+                <p class="church-loc">
+                    <a href="'$this->gmap'">'$this->city', '$this->prefecture'</a>
+                </p>
+                <p class="church-serv">
+                    Services: '$this->servday' at '$this->servtime'
+                </p>
+                </div>
+                </div>
+
+                ';
+    }
+}
+?>
+
--- church_submit.php
+++ church_submit.php
@@ -6,19 +6,16 @@
 	$dbusername = "root";
 	$dbpassword = "pitestsite";
 	$dbname = "church_db";
-	$dbtable = "staging";
+	$dbtable = "production";
 
 	$churchname = $_POST["churchname"];
 	$denomination = $_POST["denomination"];
-	$region = $_POST["region"];
-	$address1 = $_POST["address1"];
-	$address2 = $_POST["address2"];
+	$prefecture = $_POST["prefecture"];
+	$city = $_POST["city"];
 	$gmap = $_POST["gmap"];
 	$website = $_POST["website"];
 	$servday = $_POST["servday"];
 	$servtime = $_POST["servtime"];
-	$servlang = $_POST["servlang"];
-	$churchdesc = $_POST["churchdesc"];
 	$contact = $_POST["contact"];
 
 
@@ -26,19 +23,17 @@
 	try {
 	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $dbusername, $dbpassword);
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$sql = $conn->prepare("INSERT INTO $dbtable (churchname, denomination, region, address1, address2, gmap, website, servday, servtime, servlang, churchdesc)
-	VALUES (:church, :denom, :region, :add1, :add2, :gmap, :website, :servday, :servtime, :servlang, :desc)";
-	$sql->bindParam(':church', $churchname);
-	$sql->bindParam(':denom', $denomination);
-	$sql->bindParam(':region', $region);
-	$sql->bindParam(':add1', $address1);
-	$sql->bindParam(':add2', $address2);
+	$sql = $conn->prepare("INSERT INTO $dbtable (churchname, denomination, prefecture, city, gmap, website, servday, servtime, contact)
+	VALUES (:churchname, :denomination, :prefecture, :city, :gmap, :website, :servday, :servtime, :contact)");
+	$sql->bindParam(':churchname', $churchname);
+	$sql->bindParam(':denomination', $denomination);
+	$sql->bindParam(':prefecture', $prefecture);
+	$sql->bindParam(':city', $city);
 	$sql->bindParam(':gmap', $gmap);
 	$sql->bindParam(':website', $website);
 	$sql->bindParam(':servday', $servday);
 	$sql->bindParam(':servtime', $servtime);
-	$sql->bindParam(':servlang', $servlang);
-	$sql->bindParam(':churchdesc', $desc);
+	$sql->bindParam(':contact', $contact);
 
 	$sql->execute();
 	$last_id = $conn->lastInsertId();
@@ -61,11 +56,9 @@ Please allow 1-2 weeks for your submission to be reviewed.
 <ul>
 <li>Church Name: <?php echo $churchname; ?></li>
 <li>Denomination: <?php echo $denomination; ?></li>
-<li>Region: <?php echo $region ?></li>
-<li>Address: <a target="_blank" href="<?php echo $gmap; ?>"><?php echo $address1 . "<br />" . $address2; ?></a></li>
+<li>Location: <a target="_blank" href="<?php echo $gmap; ?>"><?php echo $city . ", " . $prefecture; ?></a></li>
 <li>Website: <a target="_blank" href="<?php echo $website; ?>"><?php echo $churchname; ?></a></li>
-<li>Service Information: <?php echo $servtime . ", " . $servday . ", in " . $servlang; ?></li>
-<li>Church Description: <?php echo $churchdesc; ?></li>
+<li>Service Information: <?php echo $servday . " at " . $servtime; ?></li>
 </ul>
 
 </section>
