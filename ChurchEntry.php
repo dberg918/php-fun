3ce982b7f1fc8af7e3c8eb7f3df6e59e69d5a55f
David Marshall Berg <dberg918@gmail.com>
2016-09-09 20:12:19 +0900

    fix syntax error in displayChurch method

--- ChurchEntry.php
+++ ChurchEntry.php
@@ -12,13 +12,13 @@ class ChurchEntry
     {
         print  '<div class="church-wrapper">
                 <div class="church-profile">
-                <h1><a href="'$this->website'">'$this->churchname'</a></h1>
-                <h2>'$this->denomination'</h2>
+                <h1><a href="' . $this->website . '">' . $this->churchname . '</a></h1>
+                <h2>' . $this->denomination . '</h2>
                 <p class="church-loc">
-                    <a href="'$this->gmap'">'$this->city', '$this->prefecture'</a>
+                    <a href="' . $this->gmap . '">' . $this->city . ', ' . $this->prefecture . '</a>
                 </p>
                 <p class="church-serv">
-                    Services: '$this->servday' at '$this->servtime'
+                    Services: ' . $this->servday . ' at ' . $this->servtime . '
                 </p>
                 </div>
                 </div>
